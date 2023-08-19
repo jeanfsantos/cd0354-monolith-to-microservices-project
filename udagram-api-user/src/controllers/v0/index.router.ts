3644203d62ea20df5563e8a2d1cca9ebb1455660
Jean Santos <jeanwfsantos@gmail.com>
2023-08-19 10:34:18 +0100

    removed unused module & added dockerfile

--- udagram-api-user/src/controllers/v0/index.router.ts
+++ udagram-api-user/src/controllers/v0/index.router.ts
@@ -1,10 +1,8 @@
-import { Router, Request, Response } from 'express';
-import { FeedRouter } from './feed/routes/feed.router';
+import { Request, Response, Router } from 'express';
 import { UserRouter } from './users/routes/user.router';
 
 const router: Router = Router();
 
-router.use('/feed', FeedRouter);
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
